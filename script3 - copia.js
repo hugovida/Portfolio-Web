98ed5ef83974bbf1e61b0f3293c9d023760ff7e4
Hugo Vida <hvd0001@alu.medac.es>
2024-02-28 20:17:48 +0100

    first commit

---
+++ script3 - copia.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function() {
+    var content = document.querySelector(".content");
+    var scrolled = window.scrollY;
+
+    // Aplica algún efecto o lógica según sea necesario
+    // En este ejemplo, simplemente cambiamos el color de fondo al hacer scroll
+    if (scrolled > 100) {
+      content.style.backgroundColor = "#e74c3c";
+    } else {
+      content.style.backgroundColor = "";
+    }
+  });
